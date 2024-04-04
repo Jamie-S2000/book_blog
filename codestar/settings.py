e30013ab0a27566a0498fdaa6564905369c533f5
Jamie-S2000 <jamiegsewell@gmail.com>
2024-04-04 16:25:25 +0000

    feat: readies code to be deployed to heroku

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+g57w!=b0of94o8)#g*3cw(m6$=tqf3#z!gjo#n44l5rd@jjfz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-jamie-s2000-book-blog-fth6mbifi5.us1.codeanyapp.com']
+ALLOWED_HOSTS = ['8000-jamie-s2000-book-blog-fth6mbifi5.us1.codeanyapp.com'
+                ,'.herokuapp.com']
 
 
 # Application definition
