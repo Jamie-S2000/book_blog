d2be3da56fc37972ceebd459d1988179afa68410
Jamie-S2000 <jamiegsewell@gmail.com>
2024-04-04 21:49:21 +0000

    feat: adds summernote for added admin functionality

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    list_display = ('book_title', 'slug', 'status', 'created_on')
+    search_fields = ['book_title']
+    list_filter = ('status', 'created_on')
+    prepopulated_fields = {'slug': ('book_title',)}
+    summernote_fields = ('content',)
 
 # Register your models here.
-admin.site.register(Post)
+
 admin.site.register(Comment)
