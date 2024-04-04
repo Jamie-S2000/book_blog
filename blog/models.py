d203e47a90fe72d5d7dd6e1f6b762af7b2ba4595
Jamie-S2000 <jamiegsewell@gmail.com>
2024-04-04 20:13:04 +0000

    feat: orders and gives titles to posts in admin page

--- blog/models.py
+++ blog/models.py
@@ -14,6 +14,10 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     updated_on = models.DateTimeField(auto_now=True)
+    class Meta:
+        ordering = ["-created_on"]
+    def __str__(self):
+        return f"Book title: {self.book_title} | Post by {self.author}"
 
 class Comment(models.Model):
     post = models.ForeignKey(
